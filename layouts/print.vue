7e5563fa1b6aaa355aa62749b2f340b733f53776
ONSEN <62979382+onsen-yamagata@users.noreply.github.com>
2020-04-04 23:49:46 +0900

    Update print.vue

--- layouts/print.vue
+++ layouts/print.vue
@@ -2,7 +2,7 @@
   <v-app class="app-print">
     <div v-if="loading" class="loader">
       <img src="/logo.svg" :alt="$t('山形県')" />
-      <scale-loader color="#00A040" />
+      <scale-loader color="#18428c" />
     </div>
     <v-container v-else>
       <v-row align="center" class="PrintMeta">
