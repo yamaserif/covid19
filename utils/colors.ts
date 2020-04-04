f74657dbe6243ef796fb75f0e80d359d20df2f03
ONSEN <62979382+onsen-yamagata@users.noreply.github.com>
2020-04-04 23:43:44 +0900

    Update colors.ts

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,4 +1,4 @@
-export const single: string = '#006400'
-export const double: string[] = ['#006400', '#1B75BC']
-export const triple: string[] = ['#00441B', '#006400', '#1B75BC']
-export const quadruple: string[] = ['#00441B', '#006400', '#1B75BC', '#505B00']
+export const single: string = '#008830'
+export const double: string[] = ['#008830', '#1B75BC']
+export const triple: string[] = ['#00441B', '#008830', '#1B75BC']
+export const quadruple: string[] = ['#00441B', '#008830', '#1B75BC', '#505B00']
